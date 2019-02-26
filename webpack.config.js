257105f04afb78aa823e857fc36c470c00714762
langwenda <langwdalan@gmail.com>
2019-02-26 10:18:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,5 @@ module.exports = {
     library: 'Toastly',
     libraryTarget: 'umd',
     filename: 'toastly.js'
-  },
-  module: {
-    rules: [{
-        test: /\.js/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-            presets: ['es2015'],
-            plugins:["transform-runtime"]
-        }
-    }, ],
   }
 };
