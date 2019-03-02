54ccd9572559c4cda94848fdbe3efc3066b915ee
Alan <langwdalan@gmail.com>
2019-03-02 14:52:40 +0800

    update

--- src/Toastly.tsx
+++ src/Toastly.tsx
@@ -4,14 +4,10 @@ class Message {
   duration = 3;//默认自动关闭延时，单位秒
   maxCount = 0;//最大显示数, 超过限制时，最早的消息会被自动关闭
   top = 24;//消息距离顶部的位置
+  no = Notification.create();
+
   constructor () {
-    const no = Notification.create();
-    no.create({
-      content:'1'
-    })
-    no.create({
-      content:'2'
-    })
+
   };
   /**
    * 成功消息提醒
@@ -20,7 +16,9 @@ class Message {
    * @param onClose 关闭的回调
    */
   success(content: string, duration?:number, onClose?:Function){
-    
+    this.no.create({
+      content:'1'
+    })
   }
 
   config(config:ToastlyConfigProps){
